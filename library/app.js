0fd1a1646a202031479419cecc4e4bac54b44258
CONTROLZA <controlza2021@gmail.com>
2021-08-30 16:10:35 +0300

    changes

--- library/app.js
+++ library/app.js
@@ -11,6 +11,8 @@ function submitForm(e) {
   let education = document.querySelector(".education").value;
   let business= document.querySelector(".business").value;
   let medical= document.querySelector(".medical").value;
+  let country_code= document.querySelector(".country_code").value;
+  let phone_number= document.querySelector(".phone_number").value;
   let message = document.querySelector(".message").value;
   console.log(first_name, last_name, email,education,business,medical,message);
 
@@ -18,19 +20,26 @@ function submitForm(e) {
 
   document.querySelector(".contact-form").reset();
 
-  sendEmail(first_name, last_name, email,education,business,medical,message);
+  sendEmail(first_name, last_name, email,education,business,medical,country_code,phone_number,message);
 }
 
-function sendEmail(first_name, last_name, email,education,business,medical,message) {
+function sendEmail(first_name, last_name, email,education,business,medical,country_code,phone_number,message) {
 
     Email.send({
         SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
         To : 'abdoulkowiyy2020@gmail.com',
         From : "controlza2021@gmail.com",
         Subject : `${first_name} ${last_name} sent you a message`,
-        Body : `Name: ${first_name} ${last_name} <br/> Email: ${Email} <br/> I'm interested in ${education} <br/> Message: ${Message}`
+        Body : `Name: ${first_name} ${last_name} <br/>
+         Email: ${Email} <br/> 
+         I'm interested in ${education} <br/> 
+         I'm interested in ${business} <br/>
+         I'm interested in ${medical} <br/>
+         Country Code: ${country_code} <br/> 
+         Phone Number : ${phone_number} <br/>
+         Message: ${message}`
     }).then(
-      message => alert("Mail sent Successfully")
+      (message)=> alert("Mail sent Successfully")
     );
     
 }
