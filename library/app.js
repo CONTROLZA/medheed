11fc66403e75e049bc52a718c8fb8c38a05566c8
CONTROLZA <controlza2021@gmail.com>
2021-08-30 17:23:07 +0300

    changes

--- library/app.js
+++ library/app.js
@@ -58,9 +58,9 @@ function sendEmail(
 
     Host : "smtp.gmail.com",
     Username : "controlza2021@gmail.com",
-    Password : "Ekol1927",
+    Password : "ezwzakrcquzwobhr",
     // SecureToken : "3def6893-8292-4b1b-9594-1b5226b8d044",
-    To: "abdoulkowiyy2020@gmail.com",
+    To: "controlza2021@gmail.com",
     From: "controlza2021@gmail.com",
     Subject: `${first_name} ${last_name} sent you a message`,
     Body: `Name: ${first_name} ${last_name} <br/>
