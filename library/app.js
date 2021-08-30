9668ef515fd5d7b5f2463a52712d54435ed71a11
CONTROLZA <controlza2021@gmail.com>
2021-08-30 16:30:08 +0300

    agian

--- library/app.js
+++ library/app.js
@@ -9,37 +9,66 @@ function submitForm(e) {
   let last_name = document.querySelector(".last_name").value;
   let email = document.querySelector(".email").value;
   let education = document.querySelector(".education").value;
-  let business= document.querySelector(".business").value;
-  let medical= document.querySelector(".medical").value;
-  let country_code= document.querySelector(".country_code").value;
-  let phone_number= document.querySelector(".phone_number").value;
+  let business = document.querySelector(".business").value;
+  let medical = document.querySelector(".medical").value;
+  let country_code = document.querySelector(".country_code").value;
+  let phone_number = document.querySelector(".phone_number").value;
   let message = document.querySelector(".message").value;
-  console.log(first_name, last_name, email,education,business, medical,country_code,phone_number, message);
+  console.log(
+    first_name,
+    last_name,
+    email,
+    education,
+    business,
+    medical,
+    country_code,
+    phone_number,
+    message
+  );
 
-//   saveContactInfo(name, email, message);
+  //   saveContactInfo(name, email, message);
 
-//   document.querySelector(".contact-form").reset();
+  //   document.querySelector(".contact-form").reset();
 
-  sendEmail(first_name, last_name, email,education,business,medical,country_code,phone_number,message);
+  sendEmail(
+    first_name,
+    last_name,
+    email,
+    education,
+    business,
+    medical,
+    country_code,
+    phone_number,
+    message
+  );
 }
 
-function sendEmail(first_name, last_name, email,education,business,medical,country_code,phone_number,message) {
-
-    Email.send({
-        SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
-        To : 'abdoulkowiyy2020@gmail.com',
-        From : "controlza2021@gmail.com",
-        Subject : `${first_name} ${last_name} sent you a message`,
-        Body : `Name: ${first_name} ${last_name} <br/>
+function sendEmail(
+  first_name,
+  last_name,
+  email,
+  education,
+  business,
+  medical,
+  country_code,
+  phone_number,
+  message
+) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "controlza2021@gmail.com",
+    Password: "Ekol1927",
+    // SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+    To: "abdoulkowiyy2020@gmail.com",
+    From: "controlza2021@gmail.com",
+    Subject: `${first_name} ${last_name} sent you a message`,
+    Body: `Name: ${first_name} ${last_name} <br/>
          Email: ${Email} <br/> 
          I'm interested in ${education} <br/> 
          I'm interested in ${business} <br/>
          I'm interested in ${medical} <br/>
          Country Code: ${country_code} <br/> 
          Phone Number : ${phone_number} <br/>
-         Message: ${message}`
-    }).then(
-      (message)=> alert("Mail sent Successfully")
-    );
-    
+         Message: ${message}`,
+  }).then((message) => alert("Mail sent Successfully"));
 }
